5a3994b51c4f39437d58943cbdb26a6e762a8eca
Gokhan Ipek <goekhan.ipek@tipico.com>
2021-03-07 12:02:12 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import './App.scss';
 
-function App() {
+const App = () => {
+
+  const [location, setLocation] = useState('Edirne');
+  const [weatherInfo, setWeatherInfo] = useState({})
+  const [city, setCity] = useState('')
+
+  const params = {
+    key: 'b928a896daee4e71b26225453210603',
+    location
+  }
+
+  useEffect(() => {
+    fetch(`http://api.weatherapi.com/v1/current.json?key=${params.key}&q=${params.location}`)
+      .then(response => response.json())
+      .then(data => setWeatherInfo(data))
+  }, [location])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="weather-wrapper">
+      <input 
+        onKeyDown={(event) => event.keyCode === 13 ? setLocation(city) : null } 
+        onChange={(event) => setCity(event.target.value)}
+        onBlur={() => setLocation(city)}
+         />
+      <div className="weather-card">
+        {weatherInfo.current && (
+          <>
+            <img className="weather-icon" src={weatherInfo.current.condition.icon} alt={'asdsadas'} />
+            <h1>{weatherInfo.current.temp_c}</h1>
+            <p>{weatherInfo.location.name} - <span>{weatherInfo.current.condition.text}</span></p>
+          </>
+        )
+        }
+      </div>
     </div>
   );
 }
